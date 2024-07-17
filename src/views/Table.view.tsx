26deee4dd8b42e01e196cd07a73d1d383e9931cd
Michelle More <michellemore28@gmail.com>
2024-07-17 09:26:21 -0500

    Implement table view for displaying registered questions and answers

--- src/views/Table.view.tsx
+++ src/views/Table.view.tsx
@@ -1,4 +1,21 @@
+import DeleteIcon from '@mui/icons-material/Delete'
+import EditIcon from '@mui/icons-material/Edit'
+import {
+    Box,
+    Container,
+    IconButton,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    styled,
+} from '@mui/material'
 import React from 'react'
+
+import { useAnswersStore } from '../state'
 
 // TASK 4:
 // - Implement the table from this mockup (public/table_view_mockup.png).
@@ -14,4 +31,73 @@ import React from 'react'
 // - Invoke useResetAnswers hook on delete button click.
 // - See useResetAnswers hook for more guidelines.
 
-export const TableView = () => <div id="table-view"></div>
+const TableViewWrapper = styled(Container)(({ theme }) => ({
+    display: 'flex',
+    gap: '1rem',
+    flexDirection: 'column',
+    width: '100%',
+    [theme.breakpoints.down('md')]: {
+        width: '90%',
+    },
+    [theme.breakpoints.up('md')]: {
+        width: 650,
+    },
+}))
+
+const buttonsMenuStyles: React.CSSProperties = {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    gap: '1rem',
+}
+
+export const TableView: React.FC = () => {
+    const answers = useAnswersStore(state => state.getAnswers())
+
+    const interests = answers.interests
+        .filter(elm => Object.values(elm)[0].isChecked)
+        .map(elm => Object.values(elm)[0]?.label)
+        .join(', ')
+
+    const rows = Object.entries({ ...answers, interests })
+
+    return (
+        <TableViewWrapper id="table-view">
+            <Box sx={buttonsMenuStyles}>
+                <IconButton aria-label="edit">
+                    <EditIcon />
+                </IconButton>
+                <IconButton aria-label="delete">
+                    <DeleteIcon />
+                </IconButton>
+            </Box>
+
+            <TableContainer component={Paper} sx={{ width: '100%' }}>
+                <Table width="100%" aria-label="answers table">
+                    <caption>Questions and Answers Registered</caption>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{ fontWeight: 'bold' }}>
+                                Questions
+                            </TableCell>
+                            <TableCell sx={{ fontWeight: 'bold' }}>
+                                Answers
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map(row => (
+                            <TableRow key={row[0]}>
+                                <TableCell component="th" scope="row">
+                                    {row[0]}
+                                </TableCell>
+                                <TableCell>
+                                    {row[1]?.length > 0 ? row[1] : '-'}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </TableViewWrapper>
+    )
+}
